6f27dbc78d1a7c760e4831f534994305ce5ed2e3
mayjoonjuly <mayjoonjuly>
2022-07-25 00:29:23 +0000

    using secret scope and secrets in key vault

--- set-up/mount_adls_storage.py
+++ set-up/mount_adls_storage.py
@@ -1,9 +1,14 @@
 # Databricks notebook source
 storage_account_name = "formula1newdl"
-client_id = "20edf765-f348-4138-a8a0-0cecd4ca58e7"
-tenant_id = "d15f8f49-b1d8-4b09-80a8-5e7bb0a13bba"
-client_secret = "rv58Q~CeAUBR1HcmUOKlNJxjD1O2VOE1wij~Jbq3"
-container_name = "raw"
+#help not accidently expose secrets, but not actually block someone to see the keys
+client_id            = dbutils.secrets.get(scope="formula1-scope", key="databricks-app-client-id")
+tenant_id            = dbutils.secrets.get(scope="formula1-scope", key="databricks-app-tenant-id")
+client_secret        = dbutils.secrets.get(scope="formula1-scope", key="databricks-client-secret-value")
+
+#storage_account_name = "formula1newdl"
+#client_id = "20edf765-f348-4138-a8a0-0cecd4ca58e7"
+#tenant_id = "d15f8f49-b1d8-4b09-80a8-5e7bb0a13bba"
+#client_secret = "rv58Q~CeAUBR1HcmUOKlNJxjD1O2VOE1wij~Jbq3"
 
 # COMMAND ----------
 
@@ -15,24 +20,7 @@ configs = {"fs.azure.account.auth.type": "OAuth",
 
 # COMMAND ----------
 
-#mount raw continer
-dbutils.fs.mount(
-  source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
-  mount_point = f"/mnt/{storage_account_name}/{container_name}",
-  extra_configs = configs)
-
-# COMMAND ----------
-
-#check if mounter correctly -method1
-dbutils.fs.ls("/mnt/formula1newdl/raw")
-
-# COMMAND ----------
-
-#check if mounter correctly -method2
-dbutils.fs.mounts()
-
-# COMMAND ----------
-
+#mount container
 def mount_adls(container_name):
   dbutils.fs.mount(
     source = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net/",
@@ -41,13 +29,19 @@ def mount_adls(container_name):
 
 # COMMAND ----------
 
+#mount raw container
+mount_adls("raw")
+
+# COMMAND ----------
+
 #mount processed container
 mount_adls("processed")
 
 # COMMAND ----------
 
-dbutils.fs.ls("/mnt/formula1newdl/processed")
+#listing what's in
+dbutils.fs.ls("/mnt/formula1newdl/raw")
 
 # COMMAND ----------
 
-
+dbutils.fs.ls("/mnt/formula1newdl/processed")
